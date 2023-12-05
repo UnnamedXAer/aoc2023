135f6a967ebb1d51a5684b5183cf6f944fd6ab75
UnnamedXAer <unnamedxaer.git@gmail.com>
2023-12-05 21:10:27 +0100

    update

--- day1/day1.go
+++ day1/day1.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/unnamedxaer/aoc2023/help"
+	"golang.org/x/exp/slices"
 )
 
 func Day1ex1() {
@@ -44,4 +47,90 @@ func Day1ex1() {
 		}
 	}
 	fmt.Println(sum)
+}
+
+func Day1ex2() {
+	words := []string{
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+	// digits := []byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	digits := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+	reText := "(?i)" + strings.Join(words, "|") + "|" + strings.Join(digits, "|")
+	fmt.Printf("\nre text: %q", reText)
+	re := regexp.MustCompile(reText)
+
+	ord := make([]string, 0, 1000)
+
+	file, err := os.Open("./day1/day1ex2_test.txt")
+	help.IfErr(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	sum := 0
+	fmt.Println()
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Bytes()
+
+		fmt.Printf("%50s", line)
+		ord = append(ord, fmt.Sprintf("%55s", line))
+		match := re.FindAllString(string(line), -1)
+
+		ord[i] += fmt.Sprintf("[ %-12v ]", strings.Join([]string{match[0], match[len(match)-1]}, " "))
+		fmt.Printf(" %-30v", fmt.Sprint(match))
+
+		m := match[0]
+		idx := slices.Index(words, m)
+		if idx == -1 {
+			idx = slices.Index(digits, m)
+			// b := unsafe.Slice(unsafe.StringData(m), 1)
+			// idx = bytes.Index(digits, b)
+		}
+
+		if idx == -1 {
+			fmt.Fprintf(os.Stderr, "for: %q, m = %q, index was 0", line, m)
+			panic(fmt.Sprintf("for: %q, m = %q, index was 0", line, m))
+
+			continue
+		}
+		idx += 1
+		fmt.Printf(" - [%d]", idx)
+		ord[i] += fmt.Sprintf(" - [%d]", idx)
+
+		// last number
+		lineSum := idx * 10
+		m = match[len(match)-1]
+		idx = slices.Index(words, m)
+		if idx == -1 {
+			idx = slices.Index(digits, m)
+			// b := unsafe.Slice(unsafe.StringData(m), 1)
+			// idx = bytes.Index(digits, b)
+		}
+
+		if idx == -1 {
+			fmt.Fprintf(os.Stderr, "for: %q, m = %q, index was 0", line, m)
+			panic(fmt.Sprintf("for: %q, m = %q, index was 0", line, m))
+		}
+
+		idx += 1
+		fmt.Printf(" [%d]", idx)
+		ord[i] += fmt.Sprintf(" [%d]", idx)
+		lineSum += idx
+		sum += lineSum
+		fmt.Printf(" = %d\n", lineSum)
+		ord[i] += fmt.Sprintf(" = %d\n", lineSum)
+		i++
+	}
+	slices.Sort(ord)
+	fmt.Printf("\n\n%v", ord)
+	fmt.Printf("\n sum %d", sum)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "github.com/unnamedxaer/aoc2023/day1"
 
 func main() {
-	day1.Day1ex1()
+	// day1.Day1ex1()
+	day1.Day1ex2()
 
 }
