fea079cd32c19ae2faed69b4639841681334ba76
UnnamedXAer <unnamedxaer.git@gmail.com>
2024-01-21 20:40:47 +0100

    init dat 5

---
+++ day15/day15.go
@@ -0,0 +1,33 @@
+package day15
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/unnamedxaer/aoc2023/help"
+)
+
+func extractData() (any, any) {
+
+	f, err := os.Open("./day15/data_t.txt")
+	help.IfErr(err)
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Bytes()
+	}
+
+	help.IfErr(scanner.Err())
+
+	return nil, nil
+}
+
+func Ex1() {
+	_, _ := extractData()
+}
+
+func Ex2() {
+
+}
+
